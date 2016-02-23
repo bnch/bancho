68930a98ddb11db1564108a826a31add1b7de617
Howl <git@howl.moe>
2016-02-23 22:00:53 +0100

    So I just created a package to respond to a request with a custom status code...

--- avatarserver/server.go
+++ avatarserver/server.go
@@ -6,31 +6,30 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"github.com/bnch/hsfwsc"
 )
 
 // Serve responds to an HTTP request with an avatar if present (responds with default avatar otherwise)
 func Serve(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%s a.ppy.sh%s\n", r.Method, r.URL.Path)
 	if len(r.URL.Path) < 1 {
-		defaultAvatar(w, 403)
+		defaultAvatar(w, r, 403)
 		return
 	}
 	pic := r.URL.Path[1:]
 	picID, err := strconv.Atoi(pic)
 	if err != nil {
-		defaultAvatar(w, 200)
+		defaultAvatar(w, r, 200)
 		return
 	}
 	if _, err := os.Stat("data/avatars/" + strconv.Itoa(picID) + ".png"); os.IsNotExist(err) {
-		defaultAvatar(w, 403)
+		defaultAvatar(w, r, 403)
 		return
 	}
 	http.ServeFile(w, r, "data/avatars/"+strconv.Itoa(picID)+".png")
 }
 
-func defaultAvatar(w http.ResponseWriter, statusCode int) {
-	w.WriteHeader(statusCode)
-	assetData, _ := Asset("data/default.png")
-	w.Header().Set("Content-Length", strconv.Itoa(len(assetData)))
-	w.Write(assetData)
+func defaultAvatar(w http.ResponseWriter, r *http.Request, statusCode int) {
+	//w.WriteHeader(statusCode)
+	hsfwsc.ServeFile(w, r, "data/avatars/default.png", statusCode)
 }
